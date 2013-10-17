ea0f167c4184abd97a11170b64e5df30c74eb1f8
Conrad <conrad@webtorque.co.nz>
2013-10-18 10:59:31 +1300

    Change to using config instead of statics

--- code/forms/OrderActionsForm.php
+++ code/forms/OrderActionsForm.php
@@ -21,7 +21,7 @@ class OrderActionsForm extends Form{
 			new HiddenField('OrderID', '', $order->ID)
 		);
 		$actions = new FieldList();
-		if(OrderManipulation::$allow_paying && $order->canPay() && $order->canCancel()){
+		if(Config::inst()->get('OrderManipulation', 'allow_paying') && $order->canPay() && $order->canCancel()){
 			$actions->push(new FormAction('dopayment', _t('OrderActionsForm.PAYORDER','Pay outstanding balance')));
 			$fields->push(new HeaderField("MakePaymentHeader",_t("OrderActionsForm.MAKEPAYMENT", "Make Payment")));
 			$fields->push(new LiteralField("Outstanding",
@@ -31,7 +31,7 @@ class OrderActionsForm extends Form{
 				'PaymentMethod','Payment Method',Payment::get_supported_methods(),array_shift(array_keys(Payment::get_supported_methods()))
 			));
 		}
-		if(OrderManipulation::$allow_cancelling && $order->canCancel()){
+		if(Config::inst()->get('OrderManipulation', 'allow_cancelling') && $order->canCancel()){
 			$actions->push(new FormAction('docancel', _t('OrderActionsForm.CANCELORDER','Cancel this order')));
 		}
 		parent::__construct($controller, $name, $fields, $actions);
@@ -46,7 +46,7 @@ class OrderActionsForm extends Form{
 	 * @return boolean
 	 */
 	function dopayment($data, $form) {
-		if(OrderManipulation::$allow_paying && $order = $this->Controller()->orderfromid()) {
+		if(Config::inst()->get('OrderManipulation', 'allow_paying') && $order = $this->Controller()->orderfromid()) {
 			//assumes that the controller is extended by OrderManipulation decorator
 			if($order->canPay()) {
 				// Save payment data from form and process payment
@@ -85,14 +85,14 @@ class OrderActionsForm extends Form{
 	 * @param Form $form The {@link Form} this was submitted on
 	 */
 	function docancel($data, $form) {
-		if(OrderManipulation::$allow_cancelling){
+		if(Config::inst()->get('OrderManipulation', 'allow_cancelling')){
 			$SQL_data = Convert::raw2sql($data);
 			$order = DataObject::get_by_id('Order', $SQL_data['OrderID']);
 			$order->Status = 'MemberCancelled';
 			$order->write();
 			//TODO: notify people via email?? Make it optional.
 			if(self::$email_notification){
-				$email = new Email(Email::getAdminEmail(),Email::getAdminEmail(),sprintf(_t('Order.CANCELSUBJECT','Order #%d cancelled by member'),$order->ID),$order->renderWith('Order'));
+				$email = new Email(Config::inst()->get('Email', 'admin_email'), Config::inst()->get('Email', 'admin_email'),sprintf(_t('Order.CANCELSUBJECT','Order #%d cancelled by member'),$order->ID),$order->renderWith('Order'));
 				$email->send();
 			}
 			$form->Controller()->setSessionMessage(_t("OrderForm.ORDERCANCELLED", "Order sucessfully cancelled"),'warning'); //assumes controller has OrderManipulation extension
