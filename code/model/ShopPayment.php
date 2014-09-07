545628d8b4ec203a4fec3975e37ded48d9734cac
Conrad <conrad@webtorque.co.nz>
2014-09-08 10:29:48 +1200

    fix order emails being sent for course orders

--- code/model/ShopPayment.php
+++ code/model/ShopPayment.php
@@ -1,11 +1,13 @@
 <?php
+
 /**
  * Customisations to {@link Payment} specifically
  * for the shop module.
  *
  * @package shop
  */
-class ShopPayment extends DataExtension {
+class ShopPayment extends DataExtension
+{
 
 
 	static $has_one = array(
@@ -19,13 +21,13 @@ class ShopPayment extends DataExtension {
 		"Status" => "Status"
 	);
 
-	public static function get_supported_methods(){
+	public static function get_supported_methods() {
 		return PaymentProcessor::get_supported_methods(); //Warning: this is only available on a custom version of Payment module
 	}
 
-	public static function get_method_dataset(){
+	public static function get_method_dataset() {
 		$set = new ArrayList();
-		foreach(self::get_supported_methods() as $method => $name){
+		foreach (self::get_supported_methods() as $method => $name) {
 			$set->push(new ArrayData(array(
 				'Title' => self::method_title($method),
 				'ClassName' => $method
@@ -38,15 +40,15 @@ class ShopPayment extends DataExtension {
 	/*
 	 * Return i18n string to represent payment type.
 	*/
-	public static function method_title($method){
+	public static function method_title($method) {
 		$paymentmethods = self::get_supported_methods();
-		if(isset($paymentmethods[$method])){
-			return _t("ShopPayment.".strtoupper($method),$paymentmethods[$method]);
+		if (isset($paymentmethods[$method])) {
+			return _t("ShopPayment." . strtoupper($method), $paymentmethods[$method]);
 		}
 		return $method;
 	}
 
-	public static function has_method($method){
+	public static function has_method($method) {
 		$methods = self::get_supported_methods();
 		return isset($methods[$method]);
 	}
@@ -64,12 +66,12 @@ class ShopPayment extends DataExtension {
 	 * This is called when payment status is updated in Payment.
 	 */
 	public function onAfterWrite() {
-		if($this->owner->Status == 'Success' && $order = $this->owner->Order()) {
+		if ($this->owner->Status == 'Success' && ($order = $this->owner->Order()) && $order->exists()) {
 			OrderProcessor::create($order)->completePayment();
+		} else if ($this->owner->Status == 'Incomplete' && ($order = $this->owner->Order()) && $order->exists()) {
+			OrderProcessor::create($order)->incompletePayment();
 		}
-                else if ($this->owner->Status == 'Incomplete' && $order = $this->owner->Order()) {
-                        OrderProcessor::create($order)->incompletePayment();
-                }
+		
 	}
 
 	public function redirectToOrder() {
@@ -78,18 +80,19 @@ class ShopPayment extends DataExtension {
 		return;
 	}
 
-	public function setPaidObject(DataObject $do){
+	public function setPaidObject(DataObject $do) {
 		$this->owner->PaidForID = $do->ID;
 		$this->owner->PaidForClass = $do->ClassName;
 	}
 
 	public function Status() {
-   	        return _t('Payment.'.$this->owner->Status,$this->owner->Status);
+		return _t('Payment.' . $this->owner->Status, $this->owner->Status);
 	}
 
 }
 
-class PaymentStatusUpdateField extends DropdownField{
+class PaymentStatusUpdateField extends DropdownField
+{
 
 	public function getSource() {
 		return singleton("Payment")->dbObject("Status")->enumValues();
